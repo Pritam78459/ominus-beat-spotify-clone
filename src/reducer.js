e6886fc78a06ba63f9c9dbf08e3a4f4ef1dcab7f
Pritam78459 <pritamkhamaru33@gmail.com>
2021-03-08 19:48:02 +0530

    Basic UI done

--- src/reducer.js
+++ src/reducer.js
@@ -3,7 +3,7 @@ export const initialState = {
     playlists: [],
     playing: false,
     item: null,
-    token: 'BQAIluNQSnBIyjjo2zkPHz2HrlPza8g-RCSXbfsyv1638gU_tqOkTGPFLigQTnIS6tLNwwW9ALV4YF2krjGwCt3Q9fLX0DNeCXu0M6rUMve38UFkrL6NZ-l5yaOf8pTHWt62mjLSjktkT2JCaIkwpU3NCz-vhLlZC1jzo0bZQY97MPmw',
+    //token: 'BQAIluNQSnBIyjjo2zkPHz2HrlPza8g-RCSXbfsyv1638gU_tqOkTGPFLigQTnIS6tLNwwW9ALV4YF2krjGwCt3Q9fLX0DNeCXu0M6rUMve38UFkrL6NZ-l5yaOf8pTHWt62mjLSjktkT2JCaIkwpU3NCz-vhLlZC1jzo0bZQY97MPmw',
 
 };
 
